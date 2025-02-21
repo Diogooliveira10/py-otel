ae3a0e0c214ed15c282d97cb07e525fa2fb45177
Diogooliveira10 <ddiogooliveira9@gmail.com>
2025-02-21 20:33:15 -0300

    feat: Generating logs

--- app/app.py
+++ app/app.py
@@ -1,6 +1,18 @@
+import logging
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-app = FastAPI()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+@asynccontextmanager
+async def lifespan(app):
+    logger.info('Iniciando app')
+    yield
+    logger.info('Finalizando app')
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get('/check')
 def check():
